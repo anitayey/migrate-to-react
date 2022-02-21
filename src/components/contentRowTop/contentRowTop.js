61a0de349453df5e670545c0f6e19e2d7a303ed3
anitayey <anamaria9602@gmail.com>
2022-02-21 15:49:50 -0500

    continuacion microdesafio react DH

--- src/components/contentRowTop/contentRowTop.js
+++ src/components/contentRowTop/contentRowTop.js
@@ -1,5 +1,29 @@
 import React from "react";
 import mandalorian from "../../assets/images/mandalorian.jpg"
+import ContentRowMovies from "./contentRowMovies";
+
+let contentMovies = [
+	{
+	titulo: 'MOVIES IN DATA',
+	cifra: 21,
+	color: 'primary',
+	icono: 'fa-film'
+},
+{
+	titulo: 'TOTAL AWARDS',
+	cifra: 79,
+	color: 'success',
+	icono: 'fa-award'	
+},
+{
+	titulo:'ACTORS CUANTITY',
+	cifra: 49,
+	color: 'warning',
+	icono: 'fa-user'
+	
+
+}
+]
 
 function ContentRowTop (){
     
@@ -9,62 +33,18 @@ function ContentRowTop (){
 						<h1 className="h3 mb-0 text-gray-800">App Dashboard</h1>
 					</div>
 				
-					
-					<div className="row">
-
-						<div className="col-md-4 mb-4">
-							<div className="card border-left-primary shadow h-100 py-2">
-								<div className="card-body">
-									<div className="row no-gutters align-items-center">
-										<div className="col mr-2">
-											<div className="text-xs font-weight-bold text-primary text-uppercase mb-1">Movies in Data Base</div>
-											<div className="h5 mb-0 font-weight-bold text-gray-800">21</div>
-										</div>
-										<div className="col-auto">
-											<i className="fas fa-film fa-2x text-gray-300"></i>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-
-					
-						<div className="col-md-4 mb-4">
-							<div className="card border-left-success shadow h-100 py-2">
-								<div className="card-body">
-									<div className="row no-gutters align-items-center">
-										<div className="col mr-2">
-											<div className="text-xs font-weight-bold text-success text-uppercase mb-1"> Total awards</div>
-											<div className="h5 mb-0 font-weight-bold text-gray-800">79</div>
-										</div>
-										<div className="col-auto">
-											<i className="fas fa-award fa-2x text-gray-300"></i>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-
-						<div className="col-md-4 mb-4">
-							<div className="card border-left-warning shadow h-100 py-2">
-								<div className="card-body">
-									<div className="row no-gutters align-items-center">
-										<div className="col mr-2">
-											<div className="text-xs font-weight-bold text-warning text-uppercase mb-1">Actors quantity
-											</div>
-											<div className="h5 mb-0 font-weight-bold text-gray-800">49</div>
-										</div>
-										<div className="col-auto">
-											<i className="fas fa-user fa-2x text-gray-300"></i>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
+					<div className ="row">
+						{contentMovies.map((n, index)=>{
+							return <ContentRowMovies  
+							titulo= {n.titulo}
+							cifra = {n.cifra}
+							color = {n.color}
+							icono = {n.icono}
+							key = {n.titulo.concat(index) }
+							
+						 />
+					})}
 					</div>
-					
-					
-	
 					
 					<div className="row">
 						
